b56ca319db78dd1a10c0414d24ef84f329ea4616
Asemerald <63583036+Asemerald@users.noreply.github.com>
2025-02-27 11:28:01 +0100

    Add Ui Toolkit

--- Assets/FPV/00_Scripts/Runtime/Metagame/MetagameApplication.cs
+++ Assets/FPV/00_Scripts/Runtime/Metagame/MetagameApplication.cs
@@ -11,7 +11,9 @@ namespace FPV
         internal new static MetagameApplication Instance { get; private set; }
 
         internal event Action OnReturnToMetagameAfterMatch;
-        internal bool IsServer => NetworkManager.Singleton.IsServer;
+        internal bool IsServer => NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient;
+        
+        internal bool IsHost => NetworkManager.Singleton.IsHost;
 
         protected override void Awake()
         {
