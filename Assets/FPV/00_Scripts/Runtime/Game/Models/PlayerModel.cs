3b4e771b450479a06b2534e7376efad08b1f6477
Asemerald <tdruesne05@gmail.com>
2025-05-12 16:58:51 +0200

    Update VOIP

--- Assets/FPV/00_Scripts/Runtime/Game/Models/PlayerModel.cs
+++ Assets/FPV/00_Scripts/Runtime/Game/Models/PlayerModel.cs
@@ -67,12 +67,6 @@ namespace FPV
 
         #endregion
 
-        #region VOIP
-
-        [Header("VOIP")] [SerializeField] internal GameObject _voipObject;
-
-        #endregion
-
         [Header("Runtime Values")] public Transform PickerTransform;
         public NetworkObject _NetworkObject;
         public PlayerApplication CarriedPlayer;
