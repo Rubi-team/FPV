c27b87ed90b8c6798da658e7146969ba701819b4
Asemerald <tdruesne05@gmail.com>
2025-05-14 18:58:49 +0200

    Menace v1

--- Assets/FPV/00_Scripts/Runtime/Game/Menace.cs
+++ Assets/FPV/00_Scripts/Runtime/Game/Menace.cs
@@ -7,4 +7,13 @@ public class Menace : NetworkBehaviour
 {
     private SoundPropagationSimulator MenaceListener;
     private NavMeshAgent _navMeshAgent;
+    
+    public Transform goal;
+
+    void Start () {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+    
+    
 }
