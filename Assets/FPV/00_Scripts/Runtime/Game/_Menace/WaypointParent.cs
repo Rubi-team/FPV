6aec1ee63290ff06938629cfded9215aa4558a33
Asemerald <tdruesne05@gmail.com>
2025-05-18 13:42:29 +0200

    La menace sah

---
+++ Assets/FPV/00_Scripts/Runtime/Game/_Menace/WaypointParent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class WaypointParent : MonoBehaviour
+{
+    [SerializeField] private string roomName;
+
+    public string RoomName => roomName;
+
+    private void OnDrawGizmos()
+    {
+        // Visualisation de la zone de la pièce dans l'éditeur
+        Gizmos.color = new Color(0, 1, 0, 0.2f);
+        Gizmos.DrawWireCube(transform.position, Vector3.one * 2f);
+    }
+}
