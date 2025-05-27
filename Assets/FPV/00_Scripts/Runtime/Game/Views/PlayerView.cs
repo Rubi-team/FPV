b8bb6900506ff01b0705dc37e832aa9295811c39
Asemerald <tdruesne05@gmail.com>
2025-05-27 15:30:43 +0200

    Tuez moi

--- Assets/FPV/00_Scripts/Runtime/Game/Views/PlayerView.cs
+++ Assets/FPV/00_Scripts/Runtime/Game/Views/PlayerView.cs
@@ -12,7 +12,7 @@ namespace FPV
 
         public void AudioFootsteps()
         {
-            AudioManager.Instance.PlayOneShot(AudioManager.Instance.footStep, transform.position);
+            //AudioManager.Instance.PlayOneShot(AudioManager.Instance.footStep, transform.position);
         }
     }
 }
