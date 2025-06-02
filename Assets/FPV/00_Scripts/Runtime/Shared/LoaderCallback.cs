af0999f13f6b673f0cbc9176f38687d109f018a1
Asemerald <tdruesne05@gmail.com>
2025-06-02 14:02:07 +0200

    Add Merge Check

---
+++ Assets/FPV/00_Scripts/Runtime/Shared/LoaderCallback.cs
@@ -0,0 +1,20 @@
+﻿using FPV.Shared;
+using UnityEngine;
+
+namespace FPV._00_Scripts.Runtime.Shared
+{
+    public class LoaderCallback : MonoBehaviour
+    {
+        private bool isFirstUpdate = true;
+
+        private void Update()
+        {
+            if (isFirstUpdate)
+            {
+                isFirstUpdate = false;
+                // This is the first update, so we can safely call the callback
+                SceneLoader.OnLoaderCallback();
+            }
+        }
+    }
+}
---
+++ Assets/FPV/00_Scripts/Runtime/Shared/SceneLoader.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace FPV.Shared
+{
+    public static class SceneLoader
+    {
+        private class LoadingMonoBehaviour : MonoBehaviour
+        {
+        }
+
+        private static AsyncOperation asyncLoad;
+
+        public enum Scene
+        {
+            Loading,
+            Metagame,
+            Game_Lobby,
+            Game_Main,
+            Game_Blocking,
+            Game_LevelArt
+        }
+
+        private static Action m_LoaderCallback;
+
+        public static void LoadScene(Scene scene, Action callback = null)
+        {
+            m_LoaderCallback = () =>
+            {
+                var loadingObject = new GameObject("LoadingMonoBehaviour");
+                loadingObject.AddComponent<LoadingMonoBehaviour>().StartCoroutine(LoadSceneAsync(scene));
+            };
+
+            // Load Loading Scene
+            SceneManager.LoadScene(nameof(Scene.Loading));
+        }
+
+        public static float GetLoadingProgress()
+        {
+            if (asyncLoad != null) return asyncLoad.progress;
+            return 1f;
+        }
+
+        private static IEnumerator LoadSceneAsync(Scene scene)
+        {
+            yield return null;
+
+            asyncLoad = SceneManager.LoadSceneAsync(scene.ToString());
+
+            while (asyncLoad is { isDone: false }) yield return null;
+        }
+
+
+        public static void OnLoaderCallback()
+        {
+            if (m_LoaderCallback != null)
+            {
+                m_LoaderCallback.Invoke();
+                m_LoaderCallback = null;
+            }
+            else
+            {
+                Debug.LogError("Loader callback was not set before calling OnLoaderCallback!");
+            }
+        }
+    }
+}
